2f50c179ac9d355219aa3dce04fc6a61323f9a26
kalomidin <t.kalom1994@gmail.com>
2021-01-30 18:04:41 +0900

    - DONE: 	- implemented hw4 all part - TODO: 	- make `cargo_asan` tests to pass

--- homework/src/list_set.rs
+++ homework/src/list_set.rs
@@ -1,7 +1,11 @@
 #![allow(clippy::mutex_atomic)]
-use std::cmp;
+use std::cmp::Ordering;
+use std::mem;
 use std::ptr;
 use std::sync::{Mutex, MutexGuard};
+use std::{borrow::Borrow, cmp};
+
+use mem::replace;
 
 #[derive(Debug)]
 struct Node<T> {
@@ -37,7 +41,25 @@ impl<'l, T: Ord> Cursor<'l, T> {
     /// Move the cursor to the position of key in the sorted list. If the key is found in the list,
     /// return `true`.
     fn find(&mut self, key: &T) -> bool {
-        todo!()
+        let cursor = *self.0;
+        if (cursor).is_null() {
+            return false;
+        }
+        unsafe {
+
+            // Move the cursor
+            let next_value = (*cursor).next.lock().unwrap();
+            let is_equal = (*cursor).data.cmp(key);
+
+            match is_equal {
+                Ordering::Equal => (true),
+                Ordering::Greater => (false),
+                _ => {
+                    self.0 = next_value;
+                    self.find(key)
+                }
+            }
+        }
     }
 }
 
@@ -50,24 +72,66 @@ impl<T> OrderedListSet<T> {
     }
 }
 
-impl<T: Ord> OrderedListSet<T> {
+impl<T: Ord + std::fmt::Debug> OrderedListSet<T> {
     fn find(&self, key: &T) -> (bool, Cursor<T>) {
-        todo!()
+        let value = self.head.lock().unwrap();
+        //let sample_node = Node::new(data, next)
+        let mut cursor = Cursor(value);
+        let contains = cursor.find(&key);
+        (contains, cursor)
     }
 
     /// Returns `true` if the set contains the key.
     pub fn contains(&self, key: &T) -> bool {
-        todo!()
+        let mut iter = self.iter();
+        while let Some(value) = iter.next() {
+            match value.cmp(key) {
+                Ordering::Equal => return true,
+                _ => (),
+            }
+        }
+        false
     }
 
     /// Insert a key to the set. If the set already has the key, return the provided key in `Err`.
     pub fn insert(&self, key: T) -> Result<(), T> {
-        todo!()
+        let mut head = self.head.lock().unwrap();
+        if !(*head).is_null() && unsafe { Ordering::Greater == (**head).data.cmp(&key) } {
+            let current_head = *head;
+            let mut new_node = Node::new(key, current_head);
+            mem::swap(&mut new_node, &mut (*head));
+
+            Ok(())
+        } else {
+            drop(head);
+            let (contains, mut cursor) = self.find(&key);
+            if contains {
+                return Err(key);
+            }
+
+            let next_node = *cursor.0;
+            let mut new_node = Node::new(key, next_node);
+            mem::swap(&mut new_node, &mut (*cursor.0));
+
+            Ok(())
+        }
     }
 
     /// Remove the key from the set and return it.
     pub fn remove(&self, key: &T) -> Result<T, ()> {
-        todo!()
+        let (contains, cursor) = self.find(&key);
+        if !contains {
+            return Err(());
+        }
+        let Cursor(mut cursor) = cursor;
+        unsafe {
+            let cursor_box = Box::from_raw(*cursor);
+            let value = cursor_box.data;
+            let next_cursor = *cursor_box.next.lock().unwrap();
+            let _response = mem::replace(&mut (*cursor), next_cursor );
+
+            Ok(value)
+        }
     }
 }
 
@@ -85,13 +149,39 @@ impl<'l, T> Iterator for Iter<'l, T> {
     type Item = &'l T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        match self {
+            Iter(Some(value)) => {
+                unsafe {
+                    if (*value).is_null() {
+                        self.0 = None;
+                        return None;
+                    }
+
+                    // Get the item
+                    let box_value = Box::from_raw(**value);
+                    let inner_value = Box::leak(box_value);
+                    let item: Self::Item = &inner_value.data;
+
+                    // Shift to next
+                    let next_value = inner_value.next.lock().unwrap();
+                    if next_value.is_null() {
+                        self.0 = None;
+                    } else {
+                        self.0 = Some(next_value);
+                    }
+
+                    Some(item)
+                }
+            }
+            Iter(None) => None,
+        }
     }
 }
 
 impl<T> Drop for OrderedListSet<T> {
     fn drop(&mut self) {
-        todo!()
+        let mut_mutex = self.head.get_mut().unwrap();
+        *mut_mutex = ptr::null_mut();
     }
 }
 
